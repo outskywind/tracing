6c0588d1628cc9b005f77a2e9a9425556fe0264b
chenlong <chenlong@dafy.com>
2017-06-27 18:05:13 +0800

    brave spring mvc

---
+++ skye-brave/skye-brave-springmvc/src/main/java/com/dafy/skye/brave/spring/mvc/SimpleBraveTracingInterceptor.java
@@ -0,0 +1,57 @@
+package com.dafy.skye.brave.spring.mvc;
+
+import com.github.kristofa.brave.Brave;
+import com.github.kristofa.brave.ServerResponseInterceptor;
+import com.github.kristofa.brave.http.*;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+
+/**
+ * Created by Caedmon on 2017/6/27.
+ */
+public class SimpleBraveTracingInterceptor extends HandlerInterceptorAdapter{
+    private Brave brave;
+    public SimpleBraveTracingInterceptor(Brave brave){
+        this.brave=brave;
+    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpServerRequest serverRequest=new HttpServerRequest() {
+            @Override
+            public String getHttpHeaderValue(String s) {
+                return request.getHeader(s);
+            }
+
+            @Override
+            public URI getUri() {
+                return URI.create(request.getRequestURI());
+            }
+
+            @Override
+            public String getHttpMethod() {
+                return request.getMethod();
+            }
+        };
+        brave.serverRequestInterceptor().handle(new HttpServerRequestAdapter(serverRequest, new SpanNameProvider() {
+            @Override
+            public String spanName(HttpRequest httpRequest) {
+                return httpRequest.getUri().getPath();
+            }
+        }));
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        ServerResponseInterceptor serverResponseInterceptor = this.brave.serverResponseInterceptor();
+        serverResponseInterceptor.handle(new HttpServerResponseAdapter(new HttpResponse() {
+            @Override
+            public int getHttpStatusCode() {
+                return response.getStatus();
+            }
+        }));
+    }
+}
