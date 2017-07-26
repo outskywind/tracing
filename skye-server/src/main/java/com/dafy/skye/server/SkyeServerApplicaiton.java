084ef0e56757928455af93aaac5450dcbbdf053e
quanchengyun <quanchengyun@dafy.com>
2017-07-26 21:02:12 +0800

    bugfix and optimize

--- skye-server/src/main/java/com/dafy/skye/server/SkyeServerApplicaiton.java
+++ skye-server/src/main/java/com/dafy/skye/server/SkyeServerApplicaiton.java
@@ -6,6 +6,7 @@ import com.dafy.skye.server.auto.config.EnableZipkinExtend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import zipkin.server.EnableZipkinServer;
 
 /**
@@ -22,10 +23,12 @@ import zipkin.server.EnableZipkinServer;
 public class SkyeServerApplicaiton {
     public static void main(String[] args) {
         SpringApplication application=new SpringApplication(SkyeServerApplicaiton.class);
+        //运行启动写pid
+        application.addListeners(new ApplicationPidFileWriter("SkyeServerApplicaiton.pid"));
         application.run(args);
         try {
-            //Job定时任务是非deamon，但是他此时的线程还没启动的话，会导致直接退出，
-            //因为第三方elastic 和 tomcat的线程都是deamon
+            //Job定时任务是非daemon，但是他此时的线程还没启动的话，会导致直接退出，
+            //因为第三方elastic 和 tomcat的线程都是daemon
             Thread.sleep(10000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
