42056a6e18a61d8a9ecd3d8fab1084b89fc1754e
huangguanghao <huangguanghao@dafy.com>
2018-09-13 15:18:02 +0800

    内置servlet实例用于监控

--- skye-autoconfigure/src/main/java/com/dafy/skye/autoconfigure/PrometheusListener.java
+++ skye-autoconfigure/src/main/java/com/dafy/skye/autoconfigure/PrometheusListener.java
@@ -3,8 +3,13 @@ package com.dafy.skye.autoconfigure;
 import com.dafy.skye.log.core.SkyeLogUtil;
 import com.ecwid.consul.v1.ConsulClient;
 import com.ecwid.consul.v1.agent.model.NewService;
+import com.google.common.base.Joiner;
+import io.prometheus.client.exporter.MetricsServlet;
 import io.prometheus.client.hotspot.DefaultExports;
 import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
@@ -19,10 +24,18 @@ public class PrometheusListener implements ApplicationListener<EmbeddedServletCo
 
     private static final String TAG = "app-service";
 
+    private static final String PATH_SPEC = "/prometheus";
+    private static final String CONTEXT_PATH = "/";
+    private static final int DEFAULT_PORT = 12432;
+
     @Override
     public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
         try {
             Environment env = event.getApplicationContext().getEnvironment();
+            String portStr = env.getProperty("skye.prometheus.port");
+            int appPort = StringUtils.isEmpty(portStr) ? DEFAULT_PORT : Integer.parseInt(portStr);
+            startHttpServer(appPort);
+
             String consulAddresses = env.getProperty("skye.consulServer");
             if(StringUtils.isEmpty(consulAddresses)) {
                 logger.error("skye.consulServer is invalid! skye.consulServer={}", consulAddresses);
@@ -38,9 +51,8 @@ public class PrometheusListener implements ApplicationListener<EmbeddedServletCo
                 checkInterval = env.getProperty("skye.consul-check-interval");
             }
             String appHost = SkyeLogUtil.getPrivateIp();
-            int appPort = event.getEmbeddedServletContainer().getPort();
-            String serviceId = serviceName + "-" + appHost + "-" + appPort;
-            String tcpAddr = appHost + ':' + appPort;
+            String serviceId = Joiner.on('-').join(serviceName, appHost, appPort);
+            String tcpAddr = Joiner.on(':').join(appHost, appPort);
 
             NewService newService = new NewService();
             newService.setId(serviceId);
@@ -71,5 +83,16 @@ public class PrometheusListener implements ApplicationListener<EmbeddedServletCo
         } catch (Throwable e) {
             logger.error("PrometheusListener onApplicationEvent error!", e);
         }
+    }
+
+    private void startHttpServer(int port) throws Exception {
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath(CONTEXT_PATH);
+        context.addServlet(new ServletHolder(new MetricsServlet()), PATH_SPEC);
+
+        Server server = new Server(port);
+        server.setHandler(context);
+
+        server.start();
     }
 }
